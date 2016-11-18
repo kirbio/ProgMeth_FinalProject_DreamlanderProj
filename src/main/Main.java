94e955703eb5c3d9e164c949fc6afa859b300161
sunnypwang <mariosunshinekirby@gmail.com>
2016-11-18 14:43:57 +0700

    Made it JavaFX

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,26 @@ package main;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
 import logic.Enemy;
 import logic.GameLogic;
 import logic.Player;
 import logic.Stage;
 
-public class Main {
+public class Main extends Application {
 
 	static GameLogic logic = new GameLogic();
 	
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(javafx.stage.Stage primaryStage) throws Exception {
+		//code here
 		
-		//last git test
 		logic.startGame();
-		/*while(!logic.isGameOver()) {					
-			
-		}*/
+		
 	}
 
 }
