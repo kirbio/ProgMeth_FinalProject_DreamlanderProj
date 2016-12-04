1e1cc1d276d88e9b227971d7b8508f108f83b824
kirbio <kirbio123@gmail.com>
2016-12-04 23:10:36 +0700

    implement Attack gauge (not finished)

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -14,6 +14,7 @@ public class GameLogic {
 	private static int level;
 	private static Player player;
 	private static Round round;
+	private static boolean waitforInput; //using to determine if it's wait for input or not - using mainly for AttackGuage
 	
 	public GameLogic() {
 		isGameOver = false;
@@ -53,6 +54,14 @@ public class GameLogic {
 	public static void setRound(Round round) {
 		GameLogic.round = round;
 	}
+	
+	public static boolean isWaitForInput() {
+		return waitforInput;
+	}
+	
+	public static void setWaitForInput(boolean waitforInput) {
+		GameLogic.waitforInput = waitforInput;
+	}
 
 	public void startGame() {
 		
@@ -60,73 +69,92 @@ public class GameLogic {
 			round = new Round(level);
 			round.addPlayer(player);
 			ArrayList<Enemy> enemies = round.getEnemyList();	
+			int attackpower = -1;
 
 			System.out.println("----------LEVEL "+(level+1)+"----------");
 			boolean roundEnds = false;
+			AttackGuage atkguage = new AttackGuage(2);
+			atkguage.start();
 			while(!roundEnds) {			
-				System.out.println(isAllEnemyDead());
+				//System.out.println(isAllEnemyDead());
 				RPGTextArea.text = "Press Any Key to Attack";
-				if (InputUtility.getKeyTriggered(KeyCode.ENTER)) {
-					System.out.println("PRESSSSSSSSSSSSSSSS");
-					for (Enemy e : enemies) {
-						if (!e.isDead()) {
-							player.attack(e);
-							RPGTextArea.text = "Kirby Attacks "+e.getName();
-							try {
-								Thread.sleep(500);
-							} catch (InterruptedException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-					}
-					
-					for (Enemy e : enemies) {
-						if(player.isDead()){
-							setGameOver(true);
-							System.out.println("Game Over");
-							RPGTextArea.text = "GAME OVER";
-							try {
-								Thread.sleep(500);
-							} catch (InterruptedException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-							break;
-						}
-						System.out.println(e.getName()+" "+e.getHp()+" dead : "+e.isDead());
-
-						if (!e.isDead()) {
-							e.attack(player);
-							RPGTextArea.text = e.getName()+" attacks!";
-							try {
-								Thread.sleep(500);
-							} catch (InterruptedException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						
-					}
-					
-					if(isGameOver()){
-						break;
-					}
-					
-					if(isAllEnemyDead()){
-						System.out.println("You won!");
-						RPGTextArea.text = "You won";
-						try {
-							Thread.sleep(500);
-						} catch (InterruptedException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						level++;
-						roundEnds = true;
-					}	
-					
+				if(attackpower<0){
+					waitforInput = true;
 				}
+				System.out.println(InputUtility.getKeyTriggered(KeyCode.ENTER));
+				if(InputUtility.getKeyTriggered(KeyCode.ENTER)){
+					attackpower = AttackGuage.currentAtkPower;
+					System.out.println("Attack Success, Power: "+attackpower);
+					waitforInput = false;
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					attackpower = -1;
+				}
+//				if (InputUtility.getKeyTriggered(KeyCode.ENTER)) {
+//					System.out.println("PRESSSSSSSSSSSSSSSS");
+//					for (Enemy e : enemies) {
+//						if (!e.isDead()) {
+//							player.attack(e);
+//							RPGTextArea.text = "Kirby Attacks "+e.getName();
+//							try {
+//								Thread.sleep(500);
+//							} catch (InterruptedException e1) {
+//								// TODO Auto-generated catch block
+//								e1.printStackTrace();
+//							}
+//						}
+//					}
+//					
+//					for (Enemy e : enemies) {
+//						if(player.isDead()){
+//							setGameOver(true);
+//							System.out.println("Game Over");
+//							RPGTextArea.text = "GAME OVER";
+//							try {
+//								Thread.sleep(500);
+//							} catch (InterruptedException e1) {
+//								// TODO Auto-generated catch block
+//								e1.printStackTrace();
+//							}
+//							break;
+//						}
+//						System.out.println(e.getName()+" "+e.getHp()+" dead : "+e.isDead());
+//
+//						if (!e.isDead()) {
+//							e.attack(player);
+//							RPGTextArea.text = e.getName()+" attacks!";
+//							try {
+//								Thread.sleep(500);
+//							} catch (InterruptedException e1) {
+//								// TODO Auto-generated catch block
+//								e1.printStackTrace();
+//							}
+//						}
+//						
+//					}
+//					
+//					if(isGameOver()){
+//						break;
+//					}
+//					
+//					if(isAllEnemyDead()){
+//						System.out.println("You won!");
+//						RPGTextArea.text = "You won";
+//						try {
+//							Thread.sleep(500);
+//						} catch (InterruptedException e1) {
+//							// TODO Auto-generated catch block
+//							e1.printStackTrace();
+//						}
+//						level++;
+//						roundEnds = true;
+//					}	
+//					
+//				}
 				
 				}
 				
